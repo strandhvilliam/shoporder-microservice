5ce57a868162ad18c4ed66d8d19a2075ec0b773f
villiamstrandh <strandh.villiam@gmail.com>
2023-05-23 12:14:53 +0200

    test commit in new branch

--- src/main/java/com/example/shopordermicrobackend/shopordermicrobackend/model/ShopOrder.java
+++ src/main/java/com/example/shopordermicrobackend/shopordermicrobackend/model/ShopOrder.java
@@ -12,7 +12,6 @@ import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 @Data
 @Entity
